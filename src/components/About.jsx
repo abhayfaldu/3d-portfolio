be2bce2d38b91e70a40cae378485280dbc9487ce
abhayfaldu <abhayfaldu1922@gmail.com>
2023-04-16 19:17:08 +0530

    Adding hero and about section

---
+++ src/components/About.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { motion } from "framer-motion";
+import { services } from "../constants/index.js";
+import { Tilt } from "react-tilt";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] text-center font-bold">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I'm thrilled to be a part of the dynamic world of web development. With
+        my expertise in the MERN stack, I have the skills to create stunning,
+        responsive websites that will capture your audience's attention. My
+        passion for web development drives me to stay up-to-date with the latest
+        trends and technologies, and I'm always eager to take on new challenges.
+        If you're looking for a hardworking, detail-oriented team player, I'd
+        love to talk to you about how I can contribute to your organization's
+        success.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
